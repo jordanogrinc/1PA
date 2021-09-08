e2d391f7ed6cde440b55a2c480933f3369e99178
Jordan <jordanogrinc@gmail.com>
2021-09-08 11:28:08 -0400

    Initial Commit

---
+++ 1PA/_PAApp.swift
@@ -0,0 +1,17 @@
+//
+//  _PAApp.swift
+//  1PA
+//
+//  Created by Jordan Ogrinc on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct _PAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
